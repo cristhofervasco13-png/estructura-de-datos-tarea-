c26f56df387b2d72a3cf71fd05b2c33d18a06e47
cristhofervasco13-png <cristhofervasco13@gmail.com>
2026-09-14 11:51:36 -0500

    Add NumeroPrimo class for prime number validation

--- eje.py
+++ eje.py
@@ -1,43 +1,68 @@
-## 1 validador de notas con promedio
-class Calificador:
-
+class NumeroPrimo:
+    # atributo de clase o estatico
+    # este valor s emantiene par atodas las instancias
+    primos_verficados=[]
     def __init__(self):
-        self.notas = []
+        # atributo de instancia
+        # este valor se mantiene para cada objeto creado de la clase
+        self.historial=[]
 
-    def validar_nota(self, nota):
-        if 0 <= nota <= 100:
-            return True
+    def es_primo(self,numero):
+        self.historial.append(numero)
+        primo=True
+        if numero < 2:
+            primo=False
         else:
-            return False
+            for i in range(2,numero):#(2,3,4..)
+                if numero % i == 0:
+                    primo=False
+                    break
 
-    def cargar_notas(self, *args):
-        for nota in args:
-            if self.validar_nota(nota):
-                self.notas.append(nota)
-
-        return self.notas
-
-    def promedio(self):
-        if len(self.notas) == 0:
-            return 0
-
-        suma = 0
-
-        for nota in self.notas:
-            suma = suma + nota
-
-        return suma / len(self.notas)
+        #if primo:NumeroPrimo.primos_verficados.append(numero) 
+        return primo
 
 
-c = Calificador()
-
-print(c.cargar_notas(85, 92, 110, 78, -5, 88))
-print(c.promedio()) 
-# contador de palabras unicas
-class analizador_texto:
-    def agg_palabra (palabra):
+    def primos_en_rango(self,*args):
+        primos=[]
         
-        
-## 2
+        for num in args:
+            
+            if self.es_primo(num)==True:
+                primos.append(num)
+                NumeroPrimo.primos_verficados.append(num)
+                
+      
+        return primos        
 
+    def cantidad_verificados(self):
+        return len(self.historial)
+
+    @staticmethod # accesa a los atributos de clase o estatico
+    def get_primos_verificados():
+        return len(NumeroPrimo.primos_verficados)
+     
+# instancia la clase y crea un objeto primo1(variable)   
+primo1 = NumeroPrimo()
+# para llamar a atirbutos y metodos de la clase se usa el operador punto (.)
+# prueba el método es_primo con el número 7
+if primo1.es_primo(2):
+    print("El número es primo")
+    NumeroPrimo.primos_verficados.append(2)
+else:
+    print("El número 2, no es primo")
+
+print(primo1.primos_en_rango(9,3))
+print(primo1.historial)
+print(primo1.cantidad_verificados())
+
+primo2=NumeroPrimo()
+if primo1.es_primo(5):
+    print("El número es primo")
+    NumeroPrimo.primos_verficados.append(5)
+else:
+    print("El número 5, no es primo")
+
+print(primo2.historial)
+print(NumeroPrimo.primos_verficados)
+print(NumeroPrimo.get_primos_verificados())
         
